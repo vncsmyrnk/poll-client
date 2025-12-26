dd6bb24c10c1fa2b0b5afff1ce7e2345afcbcb3b
Vinicius Mayrink <vmayrink12@gmail.com>
2025-12-26 15:25:04 -0300

    feat: adds navbar and login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { PollDetail } from './pages/PollDetail';
+import { Login } from './pages/Login';
+import { AppBar } from './components/AppBar';
+
+const Layout = () => {
+  const location = useLocation();
+  const hideAppBar = location.pathname === '/login';
+
+  return (
+    <>
+      {!hideAppBar && <AppBar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/polls/:id" element={<PollDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+};
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/polls/:id" element={<PollDetail />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+      <Layout />
     </BrowserRouter>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+export const Login = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://accounts.google.com/gsi/client";
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+      <Row className="w-100 justify-content-center">
+        <Col md={6} lg={4}>
+          <Card className="shadow">
+            <Card.Body className="text-center p-5">
+              <h2 className="mb-4">Welcome Back</h2>
+              <div id="g_id_onload"
+                data-client_id="961823302476-ftv4t3461ie5j2ep76o3nbab680gn8an.apps.googleusercontent.com"
+                data-ux_mode="redirect"
+                data-login_uri="https://poll-api.vncsmyrnk.dev/oauth/callback">
+              </div>
+              <div className="g_id_signin" data-type="standard"></div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
